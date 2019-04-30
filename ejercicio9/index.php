b43a6b2168c48a34241bd8fd624dab8723fd83c3
MIGUEL\angelmiguel <miguel.pozo@epn.edu.ec>
2019-04-30 11:26:03 -0500

    Ejercicio 9

--- ejercicio9/index.php
+++ ejercicio9/index.php
@@ -1,4 +1,7 @@
+
+
 <?php
+
 /*
 - Escribir una página en PHP que presente un calendario del mes actual.
 - Utlizar HTML y CSS para darle color y estilo al calendario. El día actual debe ser resaltado en negrita y con la celda de diferente color.
@@ -6,3 +9,41 @@
 - Pista: Utilizar tablas HTML para presentar el calendario.
 */
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calendario</title>
+    <link rel="stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/moment.min.js"></script>
+    <script src="js/fullcalendar.min.js"></script>
+    <script src="js/es.js"></script>
+    <link rel="stylesheet" href = "css/fullcalendar.min.css">
+
+
+
+</head>
+<body>
+    <div class = "container">
+        <div class="row">
+            <div class = "col"></div>
+            <div class = "col-7"><div id = "Calendario"></div></div>
+            <div class = "col"></div>
+        </div>
+    </div>
+    
+    <script>
+    
+    $(document).ready(function(){
+        $('#Calendario').fullCalendar()
+    })
+    
+    </script>
+
+
+</body>
+</html>
